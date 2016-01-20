208bb2089285ecd110646a1c6bb1a6739e31cae0
Adrian <adrianhelvik100@gmail.com>
2016-01-20 13:27:43 +0100

    ...

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
              https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Link vimrc in this folder with the one in your home folder
 ln vimrc ~/.vimrc
 
+# Install all plugins
+echo 'An error message will occur, but installation will be successful'
 vim +PlugInstall +qall
 
 # Compile Command-T
